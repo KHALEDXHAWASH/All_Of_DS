a1a9b133970b16005a47ab0b703aa34292abea8b
Mario_Saber <160775345+marioboby@users.noreply.github.com>
2025-06-09 19:57:40 +0300

    Create heap.cpp

---
+++ heap.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+class MaxHeap {
+private:
+    int* heap;
+    int size;
+    int capacity;
+
+    int parent(int i){
+        return i/2;
+    }
+    int left(int i){
+        return 2*i;
+    }
+    int right(int i){
+        return 2*i+1;
+    }
+    void swap(int& a, int& b){
+        a=a+b;
+        b=a-b;
+        a=a-b;
+    };
+    void heapify(int i){
+        int l= left(i);
+        int r= right(i);
+        int largest=i;
+        if(l<=size && heap[i]<heap[l])
+            largest=l;
+        if(r<=size && heap[largest]<heap[r])
+            largest=r;
+
+        swap(heap[i],heap[largest]);
+        if(largest==i)
+            return;
+        else
+            heapify(largest);
+    }
+    void build_heap(){
+        for(int i=size/2;i>=1;i--)
+            heapify(i);
+    }
+
+    void bubbleUp(int i) {
+        while (i > 1 && heap[parent(i)] < heap[i]) {
+            swap(heap[i], heap[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+
+public:
+    MaxHeap(int capacity):capacity(capacity),size(0){
+        heap= new int[capacity+1];
+    }
+
+    ~MaxHeap(){
+        delete []heap;
+    }
+
+    void insert(int key){
+        if (size >= capacity) return;
+        heap[++size]=key;
+        bubbleUp(size);
+    }
+    int extractMax(){
+        if(size==0) return -1;
+        int max=heap[1];
+        heap[1]=heap[size--];
+        heapify(1);
+        return max;
+    }
+    void increaseKey(int i, int newKey){
+        heap[i]=newKey;
+        bubbleUp(i);
+    }
+    int getMax(){
+        return heap[1];
+    }
+    bool isEmpty() const{
+        return !size;
+    }
+    int getSize() const{
+        return size;
+    }
+    void printHeap(int i=1){
+        printHeap(left(i));
+        cout<<heap[i]<<" ";
+        printHeap(right(i));
+    }
+};
